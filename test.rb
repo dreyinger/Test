c4cf9bea0b7af24d4ce968138b514089d5b3ab75
David Reyinger <d.reyinger@gmail.com>
2012-07-12 15:53:56 +0200

    neues branch usw.

---
+++ test.rb
@@ -0,0 +1,7 @@
+ls
+# Lists all the subdirectories in the current directory
+
+# config  id_rsa  id_rsa.pub  known_hosts
+
+mkdir key_backup
+# Makes a subdirectory called "key_backup" in the current directory
